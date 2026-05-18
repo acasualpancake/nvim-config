5d02050d68e57fd2f37ca2765dbf2f45c5baf1a0
Malte Großmann <75449341+acasualpancake@users.noreply.github.com>
2026-05-18 11:32:12 +0200

    Added blink.cmp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,33 +1,41 @@
 return {
     {
-        "mason-org/mason.nvim",
-        opts = {},
+        "mason.nvim",
+        build = ":MasonUpdate",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
-        "mason-org/mason-lspconfig.nvim",
+        "nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "blink.cmp",
+            "mason.nvim",
+        },
         config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls", "rust_analyzer", "clangd"
-                }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            -- Enable all language servers
-            vim.lsp.enable('lua_ls')
-            vim.lsp.enable('rust_analyzer')
-            vim.lsp.enable('clangd')
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-            -- Set keymaps
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+            vim.lsp.config("lua_ls", {
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            })
+
+            vim.lsp.config("rust_analyzer", {
+                capabilities = capabilities,
+            })
+
+            vim.lsp.config("clangd", {
+                capabilities = capabilities,
+            })
+
+            vim.lsp.enable({ "lua_ls", "rust_analyzer", "clangd" })
+        end,
+    },
 }
